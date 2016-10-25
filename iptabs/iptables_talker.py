0c4e1f73a0943efc69f54443a72c2dd55c2554b2
Daniele Parmeggiani <dani.parmeggiani@gmail.com>
2016-10-25 17:00:56 +0200

    Refactored module iptables_talker. It is now working, only supporting appending rules and setting the default policy. Module main now supports the normal working mode (instead of just running it with --only-parse). Added --debug, --yes and --silent options to the command line.

--- iptabs/iptables_talker.py
+++ iptabs/iptables_talker.py
@@ -14,56 +14,98 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import subprocess
+import os
 from structures import *
 
 
-class IptablesTalker:
-    LIST = "iptables --list {}"
+DEBUG_MODE = False
+
+
+LIST = "iptables -L {} -n"
+APPEND = "iptables -A {} --protocol tcp {} -j {}"  # only support TCP for now
+DEFAULT_POLICY = "iptables -P {} {}"
+CHECK = "iptables -C "
+DELETE = "workinprogress"
+
+
+class NotRootException(Exception):
     def __init__(self):
-        pass
-    
-    def make_call(self, command):
-        return subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True).decode('utf-8')
-    
-    def list_rules(self, chian=''):
-        rules = self.make_call(self.LIST.format(chain))
-        chains = []
-        while True:
-            if rules.startswith('Chain'):
-                rules = rules[rules.find(' ')+1:]
-                chain_name = rules[:rules.find(' ')]
-                rules = rules[rules.find(' ')+1:]
-                default_policy = rules[rules.find(' ')+1:rules.find(')')]
-                if default_policy == Policy.ACCEPT.value:
-                    default_policy = Policy.ACCEPT
-                elif default_policy == Policy.DROP.value:
-                    default_policy = Policy.DROP
-                elif default_policy == Policy.REJECT.value:
-                    default_policy = Policy.REJECT
-                else:
-                    raise RuntimeException("Unkown policy for chain '{}': '{}'.".format(chain_name, default_policy))
-                rules = rules[rules.find('\n')+1:]  # eliminates redundant line
-                rules = rules[rules.find('\n')+1:]  #
-                chain = Chain(chain_name)
-                chain.default_policy = default_policy
-                for i,rule in enumerate(rules.split('\n')):
-                    if rule == '':
-                        continue
-                    if rule.startswith(' '):
-                        continue
-                    action = None
-                    if rule.startswith(Policy.ACCEPT.value):
-                        action = Policy.ACCEPT
-                    elif rule.startswith(Policy.DROP.value):
-                        action = Policy.DROP
-                    elif rule.startswith(Policy.REJECT.value):
-                        action = Policy.REJECT
-                    elif rule.startswith('LOG'):
-                        action = 'LOG'
-                    else:
-                        raise RuntimeException("Unkown policy for chain '{}'#{}.".format(chain_name, i))
-                    # !!! CONTINUE FROM HERE 
-                chains.append(chain)
-            else:  # end of chains
-                break
-        return chains
+        super().__init__("You need to be root to make calls to iptables. EUID: {}.".format(os.geteuid()))
+
+
+def make_call(command):
+    if DEBUG_MODE:
+        print('[DEBUG]  $', command)
+        return
+    if os.geteuid() != 0:
+        raise NotRootException()
+    return subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True).decode('utf-8')
+
+
+def append_rule(chain, rule):
+    return make_call(APPEND.format(
+        chain,
+        "--{} {}".format(rule.command, rule.value),
+        rule.action.value + (' --log-prefix {}'.format(rule.log_prefix) if rule.log_prefix else '')
+    ))
+
+
+def append_complex_rule(chain, rule):
+    return make_call(APPEND.format(
+        chain,
+        ' '.join(["--{} {}".format(r.command, r.value) for r in rule.rules]),
+        rule.action.value + (' --log-prefix {}'.format(rule.log_prefix) if rule.log_prefix else '')
+    ))
+
+
+def set_default_policy(chain, policy):
+    return make_call(DEFAULT_POLICY.format(
+        chain, policy.value
+    ))
+
+
+def check_rule():
+    pass
+
+# def list_rules(chain=''):
+#     rules = make_call(LIST.format(chain))
+#     chains = []
+#     while True:
+#         if rules.startswith('Chain'):
+#             rules = rules[rules.find(' ')+1:]
+#             chain_name = rules[:rules.find(' ')]
+#             rules = rules[rules.find(' ')+1:]
+#             default_policy = rules[rules.find(' ')+1:rules.find(')')]
+#             if default_policy == Policy.ACCEPT.value:
+#                 default_policy = Policy.ACCEPT
+#             elif default_policy == Policy.DROP.value:
+#                 default_policy = Policy.DROP
+#             elif default_policy == Policy.REJECT.value:
+#                 default_policy = Policy.REJECT
+#             else:
+#                 raise RuntimeError("Unknown policy for chain '{}': '{}'.".format(chain_name, default_policy))
+#             rules = rules[rules.find('\n')+1:]  # eliminates redundant line
+#             rules = rules[rules.find('\n')+1:]  #
+#             chain = Chain(chain_name)
+#             chain.default_policy = default_policy
+#             for i,rule in enumerate(rules.split('\n')):
+#                 if rule == '':
+#                     continue
+#                 if rule.startswith(' '):
+#                     continue
+#                 action = None
+#                 if rule.startswith(Policy.ACCEPT.value):
+#                     action = Policy.ACCEPT
+#                 elif rule.startswith(Policy.DROP.value):
+#                     action = Policy.DROP
+#                 elif rule.startswith(Policy.REJECT.value):
+#                     action = Policy.REJECT
+#                 elif rule.startswith(Policy.LOG.value):
+#                     action = Policy.LOG
+#                 else:
+#                     raise RuntimeError("Unknown policy for chain '{}'#{}.".format(chain_name, i))
+#                 # !!! CONTINUE FROM HERE
+#             chains.append(chain)
+#         else:  # end of chains
+#             break
+#     return chains
--- iptabs/main.py
+++ iptabs/main.py
@@ -13,18 +13,50 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import sys
-import parser
 import argparse
+import parser
+import iptables_talker
+from structures import *
 
 
 def main(args):
-    if args.only_parse:
-        parser.parse_file(args.source)
+    if args.debug:
+        iptables_talker.DEBUG_MODE = True
+    parser.parse_file(args.source)
+    if not args.silent:
         print("Parsed from file '{}':".format(args.source))
         parser.print_chains()
-    else:
-        print("Work in progress.")
+    chains = parser.chains
+    if not args.only_parse:
+        if not (args.yes or args.silent):  # confirmation
+            while True:
+                try:
+                    inp = input('Are you sure you want to append these rules to iptables? [y/N] ').lower()
+                except (KeyboardInterrupt, EOFError):
+                    return 0
+                if inp in ('n', ''):
+                    return 0
+                elif inp == 'y':
+                    break
+        try:
+            for chain in chains:
+                if chains[chain].default_policy is not None:
+                    iptables_talker.set_default_policy(chain, chains[chain].default_policy)
+                for rule in chains[chain].log_rules:  # it is a best practice to put the log rules first
+                    if isinstance(rule, ComplexRule):
+                        iptables_talker.append_complex_rule(chain, rule)
+                    else:
+                        iptables_talker.append_rule(chain, rule)
+                for rule in chains[chain].rules:
+                    if isinstance(rule, ComplexRule):
+                        iptables_talker.append_complex_rule(chain, rule)
+                    else:
+                        iptables_talker.append_rule(chain, rule)
+        except iptables_talker.NotRootException:
+            print("\n\n[ERROR]  You need to be root to make calls to iptables. EUID: {}.".format(os.geteuid()))
+            return 1
     return 0
 
 
@@ -33,6 +65,11 @@ if __name__ == '__main__':
     args.add_argument('source', help="file from which read configuration")
     args.add_argument('-p', '--only-parse', help="only parse SOURCE file. Do not talk to iptables.",
                       action='store_true')
+    args.add_argument('-d', '--debug', help="run in debug mode. This will output the calls iptabs would be "
+                                            "attempting to iptables, instead of actually making them.",
+                      action='store_true')
+    args.add_argument('-y', '--yes', help="do not ask for confirmation", action='store_true')
+    args.add_argument('-s', '--silent', help="do not produce any output. Implies '-y'.", action='store_true')
     args = args.parse_args()
     sys.exit(
         main(args)
